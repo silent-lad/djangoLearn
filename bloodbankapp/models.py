0711a63c47afa8dbe093423d85541ed72a9c8fc5
DIVYANSH TRIPATHI <divyanshtripathi1998@gmail.com>
2020-03-29 20:44:06 +0530

    DB registered

--- bloodbankapp/models.py
+++ bloodbankapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Hospital(models.Model):
+    name = models.CharField(max_length=100)
+    date_created = models.DateTimeField(auto_now_add=True)
+    map_url = models.TextField()
+    city = models.TextField()
+
+
+class Appointment(models.Model):
+    hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    start_time = models.DateTimeField()
