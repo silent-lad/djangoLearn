7061e3e7ceb94b9df0d24d1b04cd8bca38c7c056
DIVYANSH TRIPATHI <divyanshtripathi1998@gmail.com>
2020-03-29 18:23:25 +0530

    Templating sorted

--- bloodbankapp/views.py
+++ bloodbankapp/views.py
@@ -3,6 +3,24 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+hospitals = [
+    {
+        'name': 'Daryaganj'
+    }, {
+        'name': 'GTB'
+    }, {
+        'name': 'MCD'
+    }
+]
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the admin portal index.")
+
+
+def test(request):
+    context = {
+        'hospitals': hospitals,
+        'title': 'Hospitals'
+    }
+    return render(request, 'bloodbankapp/dashboard.html', context)
