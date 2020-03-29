3e6cff826f1ec911c24113df0e0e2be4db025f2f
DIVYANSH TRIPATHI <divyanshtripathi1998@gmail.com>
2020-03-29 23:26:03 +0530

    user profile added

--- bloodbankapp/admin.py
+++ bloodbankapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Hospital, Appointment
+from .models import Hospital, Appointment, Profile
 
 # Register your models here.
 admin.site.register(Hospital)
 admin.site.register(Appointment)
+admin.site.register(Profile)
--- bloodbankapp/models.py
+++ bloodbankapp/models.py
@@ -4,6 +4,15 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.IntegerField()
+    is_male = models.BooleanField()
+    age = models.IntegerField()
+    weight = models.IntegerField()
+    bloodGroup = models.CharField(max_length=3)
+
+
 class Hospital(models.Model):
     name = models.CharField(max_length=100)
     date_created = models.DateTimeField(auto_now_add=True)
